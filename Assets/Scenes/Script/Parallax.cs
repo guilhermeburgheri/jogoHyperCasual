95a56ac9bd4d1097b1ad63dee9f4692ce0279f3a
guilhermeburgheri <guilherme.burgheri@hotmail.com>
2021-09-23 17:20:30 -0300

    Criando script parallax para utilizar no chão

---
+++ Assets/Scenes/Script/Parallax.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 1f;
+    [SerializeField] private float offset;
+    private Vector2 startPosition;
+    private float newXPosition;
+
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    
+    void Update()
+    {
+        newXPosition = Mathf.Repeat(Time.time * -moveSpeed, offset);
+        transform.position = startPosition + Vector2.right * newXPosition;
+    }
+}
